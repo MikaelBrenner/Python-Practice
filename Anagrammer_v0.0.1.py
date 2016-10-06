e1e24af1b225c56d6b1dad4c07f71949b367d5b2
MikaelBrenner <mikaelestavivo@gmail.com>
2016-10-06 08:08:36 -0300

    Update Anagrammer_v0.0.1.py

--- Anagrammer_v0.0.1.py
+++ Anagrammer_v0.0.1.py
@@ -8,14 +8,15 @@ letters=["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k"
 numbers=["1","2","3","4","5","6","7","8","9","0"]
 def anagrammer(list1,fWrt):
   lnt = len(list1)
+  rng = range(lnt)
   for i1 in list1:
-    for i2 in range(lnt):
-      for i3 in range(lnt):
-        for i4 in range(lnt):
-          for i5 in range(lnt):
-            for i6 in range(lnt):
-              for i7 in range(lnt):
-                for i8 in range(lnt):
+    for i2 in rng:
+      for i3 in rng:
+        for i4 in rng:
+          for i5 in rng:
+            for i6 in rng:
+              for i7 in rng:
+                for i8 in rng:
                   fWrt.write(i1 + list1[i2] + list1[i3] + list1[i4] + list1[i5] + list1[i6] + list1[i7] + list1[i8])
 def main():
   print("###Anagram generator program###")
